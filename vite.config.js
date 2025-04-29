a1c1bc81c2578cfcb3c94be927899b35f6d01b81
mgnlbb <87055049+mgnlbb@users.noreply.github.com>
2025-04-29 09:53:30 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        strictPort: true,
+        port: 5173,
+        hmr: {
+            host: 'timesheet-labib.up.railway.app',
+            protocol: 'wss',
+        },
+    },
 });
